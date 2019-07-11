383ddd0e142fd80383f8847f8a412e4d05c3efa8
Senthil_SKK <senthilkumark.nitt@gmail.com>
2019-07-11 06:43:00 +0530

    Oops concets

---
+++ Python/OOP/inheritance.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self):
+        print('Animal is created!!!')
+    
+    def who_am_i(self):
+        print("I'm an Animal")
+    
+    def eat(self):
+        print("I'm eating!!!")
+
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print("Dog created!!!")
+    
+    def who_am_i(self):
+        print("I'm a Dog...")
+
+    def bark(self):
+        print('Woof Woof!!!')
+
+myDog = Dog()
+myDog.who_am_i()
+myDog.bark()

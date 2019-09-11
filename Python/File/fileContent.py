b6c8772051da8729cc8da75c57e3299d27b547a7
Senthil_SKK <senthilkumark.nitt@gmail.com>
2019-09-11 20:57:46 +0530

    list comprehension

--- Python/File/fileContent.py
+++ Python/File/fileContent.py
@@ -1,5 +1,5 @@
 import os
-print(os.getcwd())
+'''print(os.getcwd())
 print(os.path.getsize(os.getcwd()+'/importOS.py'))
 #Define the file in Python like below
 print(os.path.getsize('/home/senthilkumar/Documents/100DaysOfCode-DataScience/Python/File/importOS.py'))
@@ -14,3 +14,9 @@ print("Total size of the Files from the directory "+os.getcwd()+":"+str(totalsiz
 
 for fileName in os.listdir(os.getcwd()):
     print(fileName+':'+str(os.path.getsize(fileName)))
+'''
+
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+capitalsItems = list(capitals.items())
+
+print(capitalsItems)

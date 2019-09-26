c2c1f1de223fe34662ee52c7829a00d44bb8828e
Senthil_SKK <senthilkumark.nitt@gmail.com>
2019-09-26 20:16:43 +0530

    Zero to Mastery Folder and files

---
+++ Python/Zero To Mastery/Basic I/StringFormat.py
@@ -0,0 +1,8 @@
+name = 'SKK'
+age = 32
+
+print(f'Hi {name}. You are {age} years old')
+
+print('Hi {}. You are {} years old'.format('SKK', 32))
+
+print('Hi {}. You are {} years old'.format(name, age))
---
+++ Python/Zero To Mastery/Basic I/StringIndex.py
@@ -0,0 +1,4 @@
+selfish = '01234567'
+
+print(selfish[0:4])
+print(selfish[::-1])

613a4262812b94116c48240efb2e060b2d2f39bd
skkthenotorious <senthilkumark.nitt@gmail.com>
2020-01-01 19:21:24 +0530

    Set and Dictionary programs are added

---
+++ Python/Concepts/Set/set.py
@@ -0,0 +1,11 @@
+mySet = set()
+print(mySet)
+
+mySet.add(1)
+print(mySet)
+
+text = 'Mississippi'
+myList = list(text)
+print(myList)
+mySet = set(myList)
+print(mySet)
---
+++ Python/Concepts/Set/setComp.py
@@ -0,0 +1,9 @@
+nums = [1,1,1,2,2,2,2,3,3,4,4,4,5,5,5,5]
+print(set(nums))
+
+mySet = set()
+
+for num in nums:
+    mySet.add(num)
+
+print(mySet)

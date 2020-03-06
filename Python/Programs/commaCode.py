7ed79b301eb1f661098dfb59c55dce5a49ab0247
skkthenotorious <senthilkumark.nitt@gmail.com>
2020-03-06 06:33:09 +0530

    Programs are added

--- Python/Programs/commaCode.py
+++ Python/Programs/commaCode.py
@@ -1,9 +1,12 @@
-spam = ['apples', 'bananas', 'tofu', 'cats']
+#spam = ['apples', 'bananas', 'tofu', 'cats']
+spam = ['apples', 'bananas']
 
 joinString =''
 
 for i in range(len(spam)):
-    if i == len(spam)-1:
+    if len(spam) == 1:
+        joinString += spam[0]
+    elif i == len(spam)-1:
         joinString += 'and ' + spam[i]
     else:
         joinString += spam[i]+', '

6db2877bf3e6c8a044cc8e81501fc07975cf74d8
skkthenotorious <senthilkumark.nitt@gmail.com>
2020-03-26 16:57:28 +0530

    Oops concepts

---
+++ Python/OOP/stack.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        self.__stackList = []
+
+    def push(self, val):
+        self.__stackList.append(val)
+
+    def pop(self):
+        val = self.__stackList[-1]
+        del self.__stackList[-1]
+        return val
+        
+    def print(self):
+        print(self.__stackList)
+
+stackObject1 = Stack()
+stackObject2 = Stack()
+
+stackObject1.push(3)
+stackObject2.push(stackObject1.pop())
+stackObject1.print()
+stackObject2.print()
+
+#print(stackObject2.pop())
